e290312d76712d2c76a01acad6f6c2302e4ab37a
Andreas Neukoetter <andreas@omni-mad.com>
2021-11-11 18:39:32 +0100

    + Create very simple music playing example for MacOS.

--- experiments/MacOS-MusicPlayer/MacOS-MusicPlayer/ViewController.m
+++ experiments/MacOS-MusicPlayer/MacOS-MusicPlayer/ViewController.m
@@ -7,12 +7,45 @@
 
 #import "ViewController.h"
 
+#import <AVFAudio/AVFAudio.h>
+
 @implementation ViewController
+
+AVAudioPlayer* player = nil;
 
 - (void)viewDidLoad {
 	[super viewDidLoad];
 
 	// Do any additional setup after loading the view.
+	// find the file
+	NSBundle *main = [NSBundle mainBundle];
+	NSString *resourcePath = [main pathForResource:@"theme-00" ofType:@"mp3"];
+	
+//	NSString* resourcePath = [NSString stringWithUTF8String:"theme-00.mp3"];
+	
+	// load data into memory
+	NSData* data = [NSData dataWithContentsOfFile:resourcePath];
+	// play music from data
+	NSError* error = [NSError alloc];
+	/*AVAudioPlayer* */player = [[AVAudioPlayer alloc]initWithData:data error:&error];
+	if( player == nil ){
+		
+	} else {
+		if( ![player prepareToPlay] ){
+			
+		} else {
+			[player setNumberOfLoops:-1];
+			if( ![player play] ){
+				
+			} else {
+				NSLog(@"Playing");
+				if( [player isPlaying] ){
+					NSLog(@"Really playing");
+					[player setVolume:0.2 fadeDuration:10.0];
+				}
+			}
+		}
+	}
 }
 
 
